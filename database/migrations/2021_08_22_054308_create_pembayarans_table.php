5bd778b41ae44c8df6cf852437d5d2a7c0394b42
Diva Mujaddidi <didid.wizard@gmail.com>
2021-08-24 17:05:12 +0700

    first commit

---
+++ database/migrations/2021_08_22_054308_create_pembayarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->unsignedBigInteger('paket_pembelajaran_id');
+            $table->foreign('paket_pembelajaran_id')->references('id')->on('paket_pembelajaran');
+            $table->bigInteger('total_bayar');
+            $table->boolean('pembayaran_selesai')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('admin.dashboard');
+});
+
+Route::get('/admin/dashboard', AdminDashboardController::class)->middleware(['auth'])->name('admin.dashboard');
+
+require __DIR__.'/auth.php';
